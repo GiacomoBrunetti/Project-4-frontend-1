d26657ff0f3e8122a154410dfabe5a684b1b5948
Giacomo Brunetti <giacomobrunetti6875@gmail.com>
2017-05-15 17:54:49 +0100

    facebook login trying to fix

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 angular
   .module('meetApp', ['ngResource', 'ui.router', 'satellizer', 'checklist-model', 'ui.bootstrap', 'ngAnimate', 'ngMessages'])
-  .constant('API_URL', 'http://localhost:3000/api')
+  .constant('API_URL', 'https://blooming-harbor-21931.herokuapp.com')
   .config(function() {
     Stripe.setPublishableKey('pk_test_49Ia4OML02cfbOn0FJuTAWuh');
   });
